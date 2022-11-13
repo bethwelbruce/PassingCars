9c4a63f1c408c861454cee7bb8b2833551ae501c
BETHWEL NELSON OTIENO <bethwelbruce@gmail.com>
2022-11-13 10:34:58 +0300

    Add files via upload

---
+++ PassingCars.py
@@ -0,0 +1,13 @@
+def solution(A):
+    sE=0
+    s=0
+    for i in range (0,len(A)):
+        if A[i]==0:
+            sE+=1
+        elif A[i]==1:
+            s+=sE
+    if s>1000000000:
+        return -1
+    else:
+        return s
+    pass
